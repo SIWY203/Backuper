c33cf6a73310604f4cbb6cc31ba5e163e54ed730
SIWY203 <cefalaspis12345@gmail.com>
2026-07-19 03:34:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+/* SZKIC PROJEKTU
+ * problemy do rozwiązania:
+ * - czy Cluster.cs ma przechowywać metody zapisu, czy ClusterManager.cs?
+ * - jak prawidłowo i gdzie połączyć listę klastrów z listą w json?
+
+#Program.cs
+    LoadClusters()
+    if cluster count = 0: ClusterCreator()
+    Menu()
+
+
+#ClusterManager.cs
+    List<Cluster> Clusters
+    LoadClusters(){}
+    ClusterCreator(){}
+    
+
+#Cluster.cs
+    Cluster(name, src, dest)
+    Load()
+    Save()
+
+
+
+*/
+
