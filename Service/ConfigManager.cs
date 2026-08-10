1433228851fdefa0183d47707cd871ff62b63e93
SIWY203 <cefalaspis12345@gmail.com>
2026-08-10 20:44:18 +0200

    .

--- Service/ConfigManager.cs
+++ Service/ConfigManager.cs
@@ -3,8 +3,12 @@
     private static readonly string AppFolder = Path.Combine(
         Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Backuper");
 
-    public static string ClusterConfigFile { get; } = Path.Combine(AppFolder, "clusters.json");
-    public static string LanguageConfigFile { get; } = Path.Combine(AppFolder, "lang_config.txt");
+    //public static string ClusterConfigFile { get; } = Path.Combine(AppFolder, "clusters.json");
+    //public static string LanguageConfigFile { get; } = Path.Combine(AppFolder, "lang_config.txt");
+
+    // temporary for no use appdata
+    public static string ClusterConfigFile = Path.Combine(AppContext.BaseDirectory, "clusters.json");
+    public static string LanguageConfigFile = Path.Combine(AppContext.BaseDirectory, "lang_config.txt");
 
     static ConfigManager()
     {
@@ -13,3 +17,5 @@
         Directory.CreateDirectory(AppFolder);
     }
 }
+    
+
