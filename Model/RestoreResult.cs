95d0f98a6c0e62f3b22e135b891fc5ae96060980
SIWY203 <cefalaspis12345@gmail.com>
2026-08-11 02:16:09 +0200

    renamed and simlified 'RestoreResult' to 'Result', because now BackupUI doesnt use TempPath, it just move temp to original dir

--- Model/RestoreResult.cs
+++
@@ -1,6 +0,0 @@
-﻿public readonly record struct RestoreResult(bool IsSuccess, string? ErrorKey = null, string? TempPath = null)
-{
-    public static RestoreResult Ok() => new(true);
-    public static RestoreResult Fail(string errorKey, string? tempPath = null) => new(false, errorKey, tempPath);
-}
-
