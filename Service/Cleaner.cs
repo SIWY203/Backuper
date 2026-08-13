e8b777dc6100930589e290f2a96e4f7761040f17
SIWY203 <cefalaspis12345@gmail.com>
2026-08-13 16:46:35 +0200

    starting work on a system for cleaning old backups

---
+++ Service/Cleaner.cs
@@ -0,0 +1,33 @@
+﻿class Cleaner
+{
+    public static int MaxBackupCount { get; set; } = 3;
+    public static int MaxSnapshotCount { get; set; } = 2;
+
+    public enum Mode { Backup, Snapshot }
+
+    public static void LoadConfig()
+    {
+        // załadować w Program.cs, wczytać configi z json, ustalić plik w ConfigManager.cs
+    }
+
+    public static void SetLimit(int limit, Mode mode)
+    {
+        if (mode == Mode.Backup) MaxBackupCount = limit;
+        if (mode == Mode.Snapshot) MaxSnapshotCount = limit;
+    }
+
+
+    public static Result Check(Cluster cluster, Mode mode)
+    {
+
+        return Result.Ok();
+    }
+
+
+    public static Result CleanUp(Cluster cluster, Mode mode)
+    {
+
+        return Result.Ok();
+    }
+
+}
